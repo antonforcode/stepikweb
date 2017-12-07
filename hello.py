a8bfe23ff3e4e476bb3f432d611d9f059023c89c
antonforcode <34097179+antonforcode@users.noreply.github.com>
2017-12-07 06:59:49 +0300

    edit

--- hello.py
+++ hello.py
@@ -3,6 +3,7 @@ bind = "0.0.0.0:8080"
 def app(environ, start_response):
     env_data = environ["QUERY_STRING"]
     data = env_data.replace("&", "\n")
+    data = bytes(data, encoding = 'utf-8')
     status = '200 OK'
     response_headers = [('Content-type','text/plain')]
     start_response(status, response_headers)
