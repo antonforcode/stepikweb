de85148abb19ab8f06b13bfad57e622977bdd998
antonforcode <34097179+antonforcode@users.noreply.github.com>
2017-12-08 23:08:19 +0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,8 +1,8 @@
 def app(environ, start_response):
     env_data = environ.get("QUERY_STRING", "")
     data = env_data.replace("&", "\n")
-    ## data = bytes(data, encoding = 'ascii')
+    data = data.encode()
     status = '200 OK'
     response_headers = [('Content-type','text/plain')]
     start_response(status, response_headers)
-    return [data]
+    return iter([data])
