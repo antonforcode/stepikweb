2e0921ddfcbf1ea1bf83f21dcf3cc84cfa4f1d11
antonforcode <kertis16@rambler.ru>
2017-12-29 13:37:09 +0000

    add

--- in.sh
+++ in.sh
@@ -1,3 +1,5 @@
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test
 sudo /etc/init.d/nginx restart
+sudo /etc/init.d/mysql start
+mysql -uroot -e "create database ask"
