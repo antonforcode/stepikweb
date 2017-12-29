084bee9a36d80738f8f124af2c4b6dc8ac1df1ae
antonforcode <34097179+antonforcode@users.noreply.github.com>
2017-12-29 10:02:42 +0300

    Update models.py

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+
+class Question(models.Model):
+    title = models.CharField(max_length=255)
+    text = models.TextField()
+    added_at = models.DateField(auto_now_add=True)
+    rating = models.IntegerField()
+    author = models.ForeignKey(django.contrib.auth.models.User)
+    likes = models.ManyToManyField(django.contrib.auth.models.User)
+    objects = QuestionManager()
+
+
+class Answer(models.Model):
+    text = models.TextField()
+    added_at = models.DateField(auto_now_add=True)
+    question = models.ForeignKey(Question)
+    author = models.ForeignKey(django.contrib.auth.models.User)
+
+
+class QuestionManager(models.Manager):
+    def new(self):
+        return self.order_by('-added_at')
+    def popular(self):
+        return self.order_by('-rating')
