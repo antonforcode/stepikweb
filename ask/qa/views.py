f1ade13d62727d3119159531421f0ee0dfcbf0ec
antonforcode <34097179+antonforcode@users.noreply.github.com>
2018-01-03 12:57:34 +0300

    Update views.py

--- ask/qa/views.py
+++ ask/qa/views.py
@@ -1,5 +1,13 @@
+"""
 from django.shortcuts import render
 from django.http import HttpResponse
 
 def test(request, *args, **kwargs):
     return HttpResponse('OK')
+"""
+
+from django.core.paginator import Paginator
+from django.shortcuts import render
+
+def main_view(request):
+    
