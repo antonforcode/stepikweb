ed2ec19cfdadf189816894e14f06cee839aba83d
antonforcode <34097179+antonforcode@users.noreply.github.com>
2018-01-04 12:39:12 +0300

    Update views.py

--- ask/qa/views.py
+++ ask/qa/views.py
@@ -6,11 +6,17 @@ def test(request, *args, **kwargs):
     return HttpResponse('OK')
 """
 
-from django.core.paginator import Paginator
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render
 
 def main_view(request):
-    page = request.GET.get('page', 1)
+    page_number = request.GET.get('page', 1)
     new_questions = Question.objects.new()
     paginator = Paginator(new_questions, 10)
     paginator.baseurl = '/?page='
+    try:
+        page = paginator.page(page_number)
+    except PageNotAnInteger:
+        page = paginator.page(1)
+    except EmptyPage:
+        page = paginator.page(paginator.num_pages)
