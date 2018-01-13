4640bf0ade33ce8414d522b2c56bd1358cd6f8a0
antonforcode <34097179+antonforcode@users.noreply.github.com>
2018-01-13 12:40:59 +0300

    Update forms.py

--- ask/qa/forms.py
+++ ask/qa/forms.py
@@ -7,8 +7,8 @@ class AskForm(forms.Form):
     text = forms.CharField(widget=forms.Textarea)
 
     def clean(self):
-        title = self.cleaned_data.get('title')
-        text = self.cleaned_data.get('text')
+        title = self.cleaned_data['title']
+        text = self.cleaned_data['text']
         if "script" in title and "script" in text:
             raise forms.ValidationError('Incorrect text')
 
@@ -24,12 +24,12 @@ class AnswerForm(forms.Form):
     question = forms.IntegerField(widget=forms.HiddenInput())
 
     def clean(self):
-        text = self.cleaned_data.get('text')
+        text = self.cleaned_data['text']
         if "script" in text:
             raise forms.ValidationError('Incorrect text')
 
     def save(self):
         self.cleaned_data['author_id'] = '1'
-        ask = Answer(**self.cleaned_data)
-        ask.save()
-        return ask
+        answer = Answer(**self.cleaned_data)
+        answer.save()
+        return answer
