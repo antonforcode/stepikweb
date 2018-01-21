1c24e5b2f238afefa7a82f11f917434dec1a92d6
antonforcode <34097179+antonforcode@users.noreply.github.com>
2018-01-21 22:54:16 +0300

    edit

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '_dr^x)ny^o49ii2spdxuey$8$at9p+(w+(iyikj=$+)2kw)f0s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
@@ -119,3 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
